a1c377c179d08cc8dffb00f8edc15d8c5513f213
ifeanyicyriacus <cyriacusifeanyi@gmail.com>
2025-10-12 10:37:25 +0100

    initialize Spring Boot project with Maven setup and basic configuration

---
+++ src/main/java/org/enumgum/EnumGumBackendApplication.java
@@ -0,0 +1,13 @@
+package org.enumgum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumGumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumGumBackendApplication.class, args);
+	}
+
+}
